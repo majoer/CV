981956bec5d2bc6be66c5428126f041dadfd1136
Mats Jørgensen <matsjoer@gmail.com>
2015-12-11 21:56:51 +0100

    Updated data-request to properly handle exceptions from the React environment

--- src/web/js/data-request.js
+++ src/web/js/data-request.js
@@ -2,9 +2,12 @@ import request from 'qwest';
 import Immutable from 'immutable';
 
 function get( cursor, url ) {
-    request.get( url ).then( function( result ) {
-        cursor.update( () => Immutable.fromJS( JSON.parse( result.response ) ) );
-    } ).catch( function( error ) {
+    request.get( url ).then( function( xhr, response ) {
+        cursor.update( () => Immutable.fromJS( response ) );
+    } ).catch( function( xhr, response, error ) {
+        if ( error === undefined ) {
+            error = xhr;
+        }
         console.log( "Error: " + error );
         cursor.update( () => Immutable.fromJS( {
             error: error
