269df43b3b19ed8c6f59ef3a7b79eaf12b445726
Mats Jørgensen <matsjoer@gmail.com>
2015-12-13 17:15:03 +0100

    Initial styling for CVTopic

--- src/web/js/cv/cvEntry.jsx
+++ src/web/js/cv/cvEntry.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import Component from 'omniscient';
 
 const CVEntry = Component('CVEntry', ({cursor}) => {
-    return <div className="cv-cvEntry">
-      <div>{cursor.get('title')}</div>
-      <div>{cursor.get('org')}</div>
-      <div>{cursor.cursor(['location', 'city']).deref()}</div>
-      <div>{cursor.get('from_date') + ' - ' + cursor.get('to_date')}</div>
+    return <div className="cv-entry">
+      <h3 className="cv-entry-title">{cursor.get('title')}</h3>
+      <h3 className="cv-entry-org">{cursor.get('org')}</h3>
+      <div className="cv-entry-city">{cursor.cursor(['location', 'city']).deref()}</div>
+      <div className="cv-entry-date">{cursor.get('from_date') + ' - ' + cursor.get('to_date')}</div>
     </div>
 });
 
