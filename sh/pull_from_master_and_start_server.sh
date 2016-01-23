526309c8b7d59c7d78cc638f9373cf86d77941df
Mats Jørgensen <matsjoer@gmail.com>
2016-01-23 16:24:23 +0100

    Updated pull script to properly check its own run-environment

--- sh/pull_from_master_and_start_server.sh
+++ sh/pull_from_master_and_start_server.sh
@@ -3,27 +3,50 @@
 # and executes server.js using Node
 
 echo 'Begin pull_from_master_and_start_server.sh'
+
+if [ ! "ls -A src" ]; then
+    echo "Please run script from project root"
+    exit
+fi
+
 PARENT_PID=$1
 COUNTER=1
+ENVIRONMENT="undefined"
 
-if ps aux grep WINPID > /dev/null; then
+if [ "$uname" == "Darwin" ]; then
+    ENVIRONMENT="OSX"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    ENVIRONMENT="Linux"
+elif [ "$(expr substr $(uname -s) 1 6)" == "CYGWIN" ]; then
+    ENVIRONMENT="Cygwin"
+elif [ ENVIRONMENT == "undefined" ]; then
+    echo "Unknown environment, shutting down"
+    exit
+fi
+
+echo "${ENVIRONMENT} environment detected"
+
+if [ ENVIRONMENT == "Cygwin"]; then
+    echo "Converting win pid to cygwin pid"
     PARENT_PID=$(ps aux | awk '{print $1, $4}' | grep $PARENT_PID | awk '{print $1}')
 fi
 
-if [ -n "$PARENT_PID" ]; then
+if [ -z "$PARENT_PID" ]; then
+    echo "Parent PID not supplied. Processing without attempting to kill parent process"
+else
     echo "Waiting for parent process (${PARENT_PID}) to close..."
 
     while [ ${COUNTER} -lt 11 ]; do
         echo "Attempt ${COUNTER} of 10"
 
-        if ps -p ${PARENT_PID} > /dev/null; then
+        if [ ps -p ${PARENT_PID} > /dev/null ]; then
             sleep 1
             COUNTER=$((COUNTER+1))
         else
             break;
         fi
     done
-    if ps -p ${PARENT_PID} > /dev/null; then
+    if [ ps -p ${PARENT_PID} > /dev/null ]; then
         echo 'Parent process is still running after 10 seconds, killing it manually...'
         kill ${PARENT_PID}
     fi
