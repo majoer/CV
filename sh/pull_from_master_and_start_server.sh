5ebb4d4dce3badc05e65e6acfb9b534ed8ee9566
Mats Jørgensen <matsjoer@gmail.com>
2016-01-24 11:11:20 +0100

    Added logging to pull script

--- sh/pull_from_master_and_start_server.sh
+++ sh/pull_from_master_and_start_server.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # This script pulls mats_jorgensen-cv from master @ github.com
 # and executes server.js using Node
-
-echo 'Begin pull_from_master_and_start_server.sh'
+LOG="/var/log/matsjorgensen.no.log"
+echo 'Begin pull_from_master_and_start_server.sh' | tee -a ${LOG}
 
 if [ ! "ls -A src" ]; then
-    echo "Please run script from project root"
+    echo "Please run script from project root" | tee -a ${LOG}
     exit
 fi
 
@@ -20,24 +20,24 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 elif [ "$(expr substr $(uname -s) 1 6)" == "CYGWIN" ]; then
     ENVIRONMENT="Cygwin"
 elif [ ENVIRONMENT == "undefined" ]; then
-    echo "Unknown environment, shutting down"
+    echo "Unknown environment, shutting down" | tee -a ${LOG}
     exit
 fi
 
-echo "${ENVIRONMENT} environment detected"
+echo "${ENVIRONMENT} environment detected" | tee -a ${LOG}
 
 if [ ENVIRONMENT == "Cygwin" ]; then
-    echo "Converting win pid to cygwin pid"
+    echo "Converting win pid to cygwin pid" | tee -a ${LOG}
     PARENT_PID=$(ps aux | awk '{print $1, $4}' | grep $PARENT_PID | awk '{print $1}')
 fi
 
 if [ -z "$PARENT_PID" ]; then
-    echo "Parent PID not supplied. Processing without attempting to kill parent process"
+    echo "Parent PID not supplied. Processing without attempting to kill parent process" | tee -a ${LOG}
 else
-    echo "Waiting for parent process (${PARENT_PID}) to close..."
+    echo "Waiting for parent process (${PARENT_PID}) to close..." | tee -a ${LOG}
 
     while [ ${COUNTER} -lt 11 ]; do
-        echo "Attempt ${COUNTER} of 10"
+        echo "Attempt ${COUNTER} of 10" | tee -a ${LOG}
 
         if [ ps -p ${PARENT_PID} > /dev/null ]; then
             sleep 1
@@ -47,19 +47,19 @@ else
         fi
     done
     if [ ps -p ${PARENT_PID} > /dev/null ]; then
-        echo 'Parent process is still running after 10 seconds, killing it manually...'
+        echo 'Parent process is still running after 10 seconds, killing it manually...' | tee -a ${LOG}
         kill ${PARENT_PID}
     fi
 fi
 
-echo 'Pulling from master...'
-git pull origin master
+echo 'Pulling from master...' | tee -a ${LOG}
+git pull origin master | tee -a ${LOG}
 
-echo 'Installing application'
-npm install
+echo 'Installing application' | tee -a ${LOG}
+npm install | tee -a ${LOG}
 
-echo 'Building Front-End'
-npm run build
+echo 'Building Front-End' | tee -a ${LOG}
+npm run build | tee -a ${LOG}
 
-echo 'Starting server'
-node src/main.js
+echo 'Starting server' | tee -a ${LOG}
+node src/main.js | tee -a ${LOG}
